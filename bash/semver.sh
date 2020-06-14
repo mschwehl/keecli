95516cc7edf414baeedf7f5d812fd9b775057f94
mschwehl <7300829+mschwehl@users.noreply.github.com>
2020-06-14 21:29:34 +0200

    naming in travis

--- bash/semver.sh
+++ bash/semver.sh
@@ -1,3 +1,5 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-cd ..
-%HOME%/.sdkman/candidates/java/current/bin/native-image.cmd --verbose --static --no-fallback -H:+ReportExceptionStackTraces -jar Main.jar main
+export SEMVER_LAST_TAG=$(git describe --abbrev=0 --tags 2>/dev/null)
+if [ -z $SEMVER_LAST_TAG ]; then
+    >&2 echo "No tags defined"
+    SEMVER_LAST_TAG="0.0.1"
+fi
